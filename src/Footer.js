3fff3857f8c5c24eca4f04f89f00d066c9f6512f
emsheesh69 <ejmntclr22@gmail.com>
2024-02-02 23:06:40 +0800

    "sikan commit"

--- src/Footer.js
+++ src/Footer.js
@@ -1,39 +1,81 @@
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer-wrapper">
-        <div className="footer-menu">
-          <div className="footer-column">
-            <div className="column-section">
-              <div className="headline" role="heading" aria-level={2}>
-                Shop for
-              </div>
-              <div className="footer-items">
-                <ul>
-                  <li>Creative Cloud</li>
-                  <li>Photoshop</li>
-                  <li>Adobe Express</li>
-                  <li>Photography</li>
-                  <li>Premiere Pro</li>
-                  <li>Adobe Stock</li>
-                  <li>Elements Family</li>
-                  <li>Document Cloud</li>
-                  <li>Acrobat</li>
-                  <li>Acrobat Signs</li>
-                  <li>Special offers</li>
-                  <li>View plans and pricing</li>
-                  <li>View all products</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="footer-column"></div>
-          <div className="footer-column"></div>
-          <div className="footer-column"></div>
-          <div className="footer-column"></div>
+    <footer>
+      <div className="footer-columns">
+        <div className="footer-column">
+          <h4>Shop for</h4>
+          <ul>
+            <li>Creative Cloud</li>
+            <li>Photoshop</li>
+            <li>Adobe Express</li>
+            <li>Photography</li>
+            <li>Premiere Pro</li>
+            <li>Adobe Stock</li>
+            <li>Elements Family</li>
+            <li>Document Cloud</li>
+            <li>Acrobat</li>
+            <li>Acrobat Sign</li>
+            <li>Special offers</li>
+            <li>View plans and pricing</li>
+            <li>View all products</li>
+          </ul>
+        </div>
+
+        <div className="footer-column">
+          <h4>For business</h4>
+          <ul>
+            <li>Creative Cloud for business</li>
+            <li>Creative Cloud for enterprise</li>
+            <li>Acrobat for Business</li>
+            <li>For education</li>
+            <li>Discounts for students and teachers</li>
+            <li>Schools and universities</li>
+            <li>Digital Learning Solutions</li>
+            <li>For mobile</li>
+            <li>Apps for iOS</li>
+            <li>Apps for Android</li>
+          </ul>
+        </div>
+
+        <div className="footer-column">
+          <h4>Experience Cloud</h4>
+          <ul>
+            <li>What is Experience Cloud?</li>
+            <li>Analytics</li>
+            <li>Experience Manager</li>
+            <li>Commerce</li>
+            <li>Marketo Engage</li>
+            <li>Workfront</li>
+          </ul>
+        </div>
+        <div className="footer-column">
+          <h4>Support</h4>
+          <ul>
+            <li>Download and install</li>
+            <li>Help Center</li>
+            <li>Adobe Support Community</li>
+            <li>Enterprise Support</li>
+            <li>Genuine software</li>
+          </ul>
+        </div>
+
+        <div className="footer-column">
+          <h4>Adobe</h4>
+          <ul>
+            <li>About</li>
+            <li>Careers</li>
+            <li>Newsroom</li>
+            <li>Corporate responsibility</li>
+            <li>Investor Relations</li>
+            <li>Supply chain</li>
+            <li>Trust Center</li>
+            <li>Events</li>
+            <li>Diversity and inclusion</li>
+            <li>Integrity</li>
+          </ul>
         </div>
       </div>
-    </div>
+    </footer>
   );
 }
 export default Footer;
